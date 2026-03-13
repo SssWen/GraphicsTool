d90aad323543ad5bf9366cc72990fc8e9d8b7aeb
JerryWen <939335247@qq.com>
2026-03-13 14:22:56 +0800

    修改apk判断配置

--- ZygiskDobbyHook/module/src/main/cpp/game.h
+++ ZygiskDobbyHook/module/src/main/cpp/game.h
@@ -4,11 +4,5 @@
 // TODO: change this
 // #define TargetLibName "libil2cpp.so"
 #define TargetLibName "libgui-plugin.so"
-// #define GamePackageName "com.gzgroup.lproject3"
-#define GamePackageName "com.jzstudio.msw"
-#define GamePackageName1 "com.blade.bd"
-#define GamePackageName2 "com.run.tower.defense"
-#define GamePackageName3 "com.funplus.ts.global"
-#define GamePackageName4 "com.hypergryph.endfield"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
--- ZygiskDobbyHook/module/src/main/cpp/main.cpp
+++ ZygiskDobbyHook/module/src/main/cpp/main.cpp
@@ -1,5 +1,7 @@
 #include <cstring>
 #include <thread>
+#include <vector>
+#include <fstream>
 #include "hack.h"
 #include "zygisk.hpp"
 #include "game.h"
@@ -37,13 +39,39 @@ private:
     JNIEnv *env;
     bool enable_hack;
     char *game_data_dir;
+    std::vector<std::string> allowedPackages;
+    bool configLoaded = false;
+
+    bool loadConfig(const char* configPath) {
+        std::ifstream configFile(configPath);
+        if (!configFile.is_open()) {            
+            return false;
+        }
+        std::string line;
+        while (std::getline(configFile, line)) {
+            if (line.empty() || line[0] == '#') continue;
+            line.erase(line.find_last_not_of(" \n\r\t") + 1);
+            allowedPackages.emplace_back(line);
+        }
+        configFile.close();
+        return true;
+    }
 
     void preSpecialize(const char *package_name, const char *app_data_dir) {
-        if (strcmp(package_name, GamePackageName) == 0 || strcmp(package_name, GamePackageName1) == 0 || strcmp(package_name, GamePackageName2) == 0 || strcmp(package_name, GamePackageName3) == 0 || strcmp(package_name, GamePackageName4) == 0) {
-            // LOGI("detect game: %s", package_name);
+        if (!configLoaded) {
+            loadConfig("/data/local/tmp/renderdoc.cfg");
+            configLoaded = true;
+        }
+        bool is_allowed = std::any_of(allowedPackages.begin(), allowedPackages.end(),
+            [package_name](const std::string& pkg) {
+                return pkg == package_name;
+            });
+        if (is_allowed) {            
             enable_hack = true;
             game_data_dir = new char[strlen(app_data_dir) + 1];
             strcpy(game_data_dir, app_data_dir);
+        } else {
+            enable_hack = false;
         }
     }
 };
